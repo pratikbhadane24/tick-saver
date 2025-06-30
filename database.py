ef39d4eee77dcc572752f57496e5d3d3f4949223
Pratik Bhadane <pratikbhadane24@gmail.com>
2025-06-30 09:20:27 +0530

    feat: Update Redis connection to use CANDLE_REDIS_URL environment variable

--- database.py
+++ database.py
@@ -10,6 +10,7 @@ redis_host = os.environ.get('REDIS_HOST', 'localhost')
 redis_port = os.environ.get('REDIS_PORT', '6379')
 redis_password = os.environ.get('REDIS_PASSWORD', None)
 redis_db_no = os.environ.get('REDIS_DB_NO', 0)
+candle_redis_uri = os.environ.get('CANDLE_REDIS_URL')
 
 try:
     REDIS_DB_CLIENT = redis.Redis(host=redis_host, port=int(redis_port), password=redis_password,
@@ -19,6 +20,6 @@ except Exception as e:
 
 
 try:
-    REDIS_DATA_STORE = redis.from_url("redis://localhost:6379/0")
+    REDIS_DATA_STORE = redis.from_url(candle_redis_uri)
 except Exception as e:
     logging.error(f"Error connecting to Redis DB: {e}")
